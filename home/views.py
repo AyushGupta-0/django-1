f2ad85b3620547e990a4eb948b6ef8ca96f772b8
Ayush <ayush5555.ag@gmail.com>
2023-12-30 13:31:46 +0530

    simple makemigratiions and migrate

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render ,HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 def index(request):
     context = {
@@ -16,4 +17,12 @@ def services(request):
     return render(request , "services.html")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        contact = Contact(name=name,phone=phone,email=email,password=password,date = datetime.today())
+        contact.save()
+    
     return render(request , "contact.html")
